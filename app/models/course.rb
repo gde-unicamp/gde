80f5b4d019ebcfa6291c5f2afefc71a5efdd7978
oesgalha <oscaresgalha@gmail.com>
2014-09-25 00:37:53 -0300

    Fixed typo

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ActiveRecord::Base
-  enum terms: [ :first_semester, :second_semester, :summer_vacations ]
+  enum term: [ :first_semester, :second_semester, :summer_vacations ]
 
   belongs_to :faculty
   belongs_to :professor
