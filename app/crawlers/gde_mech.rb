ae205b50b52222610a222d588e49526e1cccdc2b
oesgalha <oscaresgalha@gmail.com>
2014-10-08 21:49:39 -0300

    Added documentation for GdeMech

--- app/crawlers/gde_mech.rb
+++ app/crawlers/gde_mech.rb
@@ -1,15 +1,33 @@
 require 'mechanize'
 
+#
+# A Mech class that should be extended to make crawlers.
+# Here you'll find some auxiliar methods to extract data from HTML
+#
 class GdeMech < Mechanize
 
+  # Extract a clean string from a node founded by the given xpath
+  #
+  # @param xpath [String] the xpath with the string location
+  # @return [String] a clean string extracted from the HTML
   def extract_str(xpath)
     clean_str(page.at(xpath).text)
   end
 
+  # Extract an integer from a node founded by the given xpath
+  #
+  # @param xpath [String] the xpath with the integer location
+  # @return [Integer] the integer extracted from the html
   def extract_int(xpath)
     page.at(xpath).text[/\d+/].to_i
   end
 
+  # Cleans a string, removing trailing spaces, converting all
+  # characters to UTF-8, removing duplicated spaces and
+  # removing \xC2\xA0 characters
+  #
+  # @param string [String] any string
+  # @return [String] the clean version of the string
   def clean_str(string)
     string.mb_chars.tidy_bytes.gsub(/(\s{2,}|\u00a0)/, ' ').strip.to_s
   end
