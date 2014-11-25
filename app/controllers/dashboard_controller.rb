969659ae6c80c8691ddf6416876a6233ac4c1ad9
oesgalha <oscaresgalha@gmail.com>
2014-11-25 21:13:46 -0200

    Testing. Showing some courses data on dashboard.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class DashboardController < ApplicationController
   def feed
   end
+
+  def courses
+    @courses = Course.first(10)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   unauthenticated do
     root to: 'home#index'
   end
+  get 'cursos' => 'dashboard#courses', as: 'courses'
 end
