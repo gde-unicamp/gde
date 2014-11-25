e2c82bd611288a3983401749489dcbcb874fd992
oesgalha <oscaresgalha@gmail.com>
2014-11-25 20:24:22 -0200

    Squeeze RoomReservations together when they are in the same room and in a row.

--- app/crawlers/room_reservation_parser.rb
+++ app/crawlers/room_reservation_parser.rb
@@ -6,22 +6,40 @@ class RoomReservationParser
     'Qua' => 2,
     'Qui' => 3,
     'Sex' => 4
-  }
+  }.freeze
 
   def initialize(raw, offering)
     @offering = offering
     data = raw.split(/[\s\n]/).map { |s| s.empty? ? nil : s }.compact
     @raw_day_of_week = data[0]
-    @raw_reservations = data[1..-1].map { |res| res.split(/\//) }
+    @raw_reservations = data[1..-1].map do |res|
+      time_and_room = res.split(/\//)
+      {
+        initial_time: time_and_room[0].to_time,
+        room: time_and_room[1],
+        duration: 1.hour
+      }
+    end
+    i = 0
+    while i < @raw_reservations.size - 1
+      if @raw_reservations[i] && @raw_reservations[i][:room] == @raw_reservations[i+1][:room] &&
+        (@raw_reservations[i+1][:initial_time] - @raw_reservations[i][:duration] - @raw_reservations[i][:initial_time] <= 0.01)
+        @raw_reservations[i][:duration] += 1.hour
+        @raw_reservations.delete_at(i+1)
+        i = 0
+      else
+        i += 1
+      end
+    end
   end
 
   def room_reservations
     @raw_reservations.map do |res|
       RoomReservation.where(
         day_of_week: day_of_week,
-        start_time: res.first.to_time,
-        end_time: res.first.to_time + 1.hour,
-        classroom: Classroom.find_by(name: res.last) || Classroom.create!(name: res.last),
+        start_time: res[:initial_time],
+        end_time: res[:initial_time] + res[:duration],
+        classroom: Classroom.find_or_create_by(name: res[:room]),
         offering: @offering
       ).first_or_create
     end
