72449ef346ef8356f6dce9cfea8617123980b7e0
Fábio Beranizo <fabio.beranizo@gmail.com>
2015-01-26 19:16:31 -0200

    Adds friends search box/Renames sass files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Creates a user for test purposes
+User.new(
+  name: "Fábio Beranizo Fontes Lopes", 
+  email: "f104864@dac.unicamp.br", 
+  password: "passwd", 
+  password_confirmation: "passwd"
+).save(validate: false)
